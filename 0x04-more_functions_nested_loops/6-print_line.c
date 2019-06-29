ca9cc3ead319597558a3287e3b3386fa923bd2bb
char1esincharge <char1esincharge@outlook.com>
2019-06-29 01:44:24 +0000

    print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include"holberton.h"
+/**
+ * prints a straight line
+ *
+ * return 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
